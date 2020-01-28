719c7d156413d057326c8c1f40b051580c5e798b
GaelleMarais <marais.gaelle@gmail.com>
2020-01-28 11:43:06 +0100

    Animations and states

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject target;
+    public Transform healthBarImage;
+    Animator animator;
+    public float attackFrequence = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        StartCoroutine(AttackLoop());
+    }
+
+
+    IEnumerator AttackLoop()
+    {
+        while (!animator.GetBool("dead"))
+        {
+            yield return new WaitForSeconds(2);
+            animator.SetTrigger("attacking");
+            yield return new WaitForSeconds(1.5f);
+            target.GetComponent<HealthManager>().TakeDamage(2);
+            yield return new WaitForSeconds(attackFrequence);
+        }
+    }
+}
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public Transform healthBarImage;
+    public GameObject target;
+    public float health = 13.3f;
+    private float healthBar = 13.3f;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        StartCoroutine(UpdateBar());
+
+        if(health <= 0){
+            gameObject.GetComponent<Animator>().SetBool("dead", true);
+            target.GetComponent<Animator>().SetBool("victory", true);
+        }
+    }
+
+    IEnumerator UpdateBar()
+    {
+        while (healthBar > health)
+        {
+            healthBar -= 0.1f;
+            if(healthBar >= 0)
+            {
+                healthBarImage.localScale = new Vector3(healthBar, 0.2f, 0.2f);
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield return null;
+    }
+
+}
